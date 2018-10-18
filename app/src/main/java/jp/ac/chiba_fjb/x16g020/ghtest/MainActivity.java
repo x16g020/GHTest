9840a87659dc33d16a5a6c3d6032e38cdf23c019
竹　力也 <x16g020@chiba-fjb.ac.jp>
2018-10-18 18:49:19 +0900

    GHTest初回コミット

---
+++ app/src/main/java/jp/ac/chiba_fjb/x16g020/ghtest/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.chiba_fjb.x16g020.ghtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
